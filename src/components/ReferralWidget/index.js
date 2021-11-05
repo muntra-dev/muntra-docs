6bfd7ebd3e5a5a98dc738f81eb004bb57ecb8c45
Filip Heinel <77172584+Wilkrew@users.noreply.github.com>
2021-11-05 16:29:23 +0100

    Progress Save

--- src/components/ReferralWidget/index.js
+++ src/components/ReferralWidget/index.js
@@ -30,6 +30,50 @@ function ReferralWidget() {
         </div>
         <div class='muntra-referral-widget' muntra_clinic_id='1'></div>
       </div>
+      <div class="markdown-container ltr">
+      <div class="markdown-content">
+        <p>Muntra referral react widget</p>
+        <h1 id="placing-a-widget-on-a-page">Placing a widget on a page</h1>
+        <p>First, place the</p>
+        <div>
+          tag in the site markup, with the following attributes:
+          <p></p>
+          <p>Required attributes:</p>
+          <ul>
+            <li><code>class</code>: "muntra-referral-widget",</li>
+            <li>
+              <p><code>muntra_clinic_id</code>: "clinic_id".</p>
+              <p>Example:</p>
+              <pre><code class=" hljs ">&lt;div
+ <span class="hljs-class"><span class="hljs-keyword">class</span></span>=<span class="hljs-string">"muntra-referral-widget"</span>
+ muntra_clinic_id=<span class="hljs-string">"1"</span>
+/&gt;
+</code></pre>
+              <p>Secondly, place a tag:</p>
+              <pre><code class=" hljs "><span class="hljs-tag">&lt;<span class="hljs-name">script</span> <span class="hljs-attr">type</span> = <span class="hljs-string">"text/javascript"</span> <span class="hljs-attr">src</span> = <span class="hljs-string">"https://muntra-dev.github.io/referral-page/index.js"</span>&gt;</span><span class="undefined"> </span><span class="hljs-tag">&lt;/<span class="hljs-name">script</span>&gt;</span>
+</code></pre>
+              <p>At the end of the body, to connect the widget to the page.</p>
+            </li>
+          </ul>
+          <p>Expample of connecting widget to a page:</p>
+          <pre><code class=" hljs "> <span class="hljs-tag">&lt;<span class="hljs-name">body</span>&gt;</span>
+   <span class="hljs-tag">&lt;<span class="hljs-name">div</span>
+     <span class="hljs-attr">class</span>=<span class="hljs-string">"muntra-referral-widget"</span>
+     <span class="hljs-attr">muntra_clinic_id</span>=<span class="hljs-string">"1"</span>
+   &gt;</span><span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
+   <span class="hljs-tag">&lt;<span class="hljs-name">script</span> <span class="hljs-attr">type</span>=<span class="hljs-string">"text/javascript"</span> <span class="hljs-attr">src</span>=<span class="hljs-string">"https://muntra-dev.github.io/referral-page/index.js"</span>&gt;</span><span class="undefined"></span><span class="hljs-tag">&lt;/<span class="hljs-name">script</span>&gt;</span>
+ <span class="hljs-tag">&lt;/<span class="hljs-name">body</span>&gt;</span>
+</code></pre>
+          <h1 id="styling-a-widget-window">Styling a widget window</h1>
+          <p>Use muntra-referral-widget styles to style the widget window:</p>
+          <pre><code class=" hljs "><span class="hljs-selector-class">.muntra-referral-widget</span> {
+  <span class="hljs-attribute">height</span>: <span class="hljs-number">1000px</span>;
+  <span class="hljs-attribute">margin</span>: <span class="hljs-number">10px</span> auto <span class="hljs-number">0</span>;
+}
+</code></pre>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
