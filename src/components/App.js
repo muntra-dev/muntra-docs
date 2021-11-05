91a4432664a3201d75c347938960c001d4bd7538
Filip Heinel <77172584+Wilkrew@users.noreply.github.com>
2021-11-05 11:48:00 +0100

    Boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import "../App.css";
+
+function App() {
+  return <div>Here we will have stuff!</div>;
+}
+
+export default App;
---
+++ src/components/BookingWidget/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+function BookingWidget() {}
+
+export default BookingWidget;
